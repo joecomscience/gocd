bfc4c06f5af4fc2d088ec8aba757e1ae059213d0
joewalker <assassin.joe@hotmail.com>
2020-01-29 11:08:36 +0700

    update slave

--- slave/entrypoint.sh
+++ slave/entrypoint.sh
@@ -12,6 +12,8 @@ git \
 docker
 
 echo '--check passwd--'
+# useradd ${USER}
+# sed -i -e 's/#PermitRootLogin/PermitRootLogin/g' sshd_config
 echo "${USER}:${PASS}" | chpasswd -c SHA512 
 
 echo '--gen key--'
@@ -25,7 +27,18 @@ source "${HOME}/.sdkman/bin/sdkman-init.sh"
 sdk version
 
 echo '--install dependency--'
-java_version="8.0.232.hs-adpt"
+java_version="8.0.242.hs-adpt"
 
 sdk install java ${java_version}
 sdk install maven
+
+mkdir -p /usr/local/java
+ln -s ~/.sdkman/candidates/java/current/bin/ /usr/local/java/bin
+
+echo '--install sonar-scanner--'
+
+curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip -o sonar-scanner-cli-4.2.0.1873-linux.zip
+unzip sonar-scanner-cli-4.2.0.1873-linux.zip -d /home
+rm -rf sonar-scanner-cli-4.2.0.1873-linux.zip
+cd /home
+mv sonar-scanner-4.2.0.1873-linux sonar-scanner
