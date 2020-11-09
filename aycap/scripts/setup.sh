0b94cc067661becd362c511d376d9e122eb85cdf
joewalker <assassin.joe@hotmail.com>
2020-11-09 16:14:58 +0700

    create docker build slave mutiple java version

--- aycap/scripts/setup.sh
+++ aycap/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-chmod u+x transformxml
+chmod u+x ./libs/transformxml
 mv ./libs/transformxml /usr/local/bin
 
 mv maven_settings.xml /usr/share/java/maven-3/conf/settings.xml
@@ -11,25 +11,19 @@ mv ca.crt /usr/local/share/ca-certificates/ca.crt
 
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.30-r0/glibc-2.30-r0.apk
-apk add glibc-2.30-r0.apk
+apk add --no-cache glibc-2.30-r0.apk
 
 echo '--install openjfx--'
 wget --quiet --output-document=/etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-java-openjfx/releases/download/8.151.12-r0/java-openjfx-8.151.12-r0.apk
 apk add --no-cache java-openjfx-8.151.12-r0.apk
 
-echo '--install sonar-scanner--'
-wget -O sonar-scanner-cli-4.2.0.1873.zip  https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873.zip
-unzip sonar-scanner-cli-4.2.0.1873.zip -d /home
-rm -f sonar-scanner-cli-4.2.0.1873.zip
-cd /home
-mv sonar-scanner-4.2.0.1873 sonar-scanner
-
-echo '--install dependency check--'
-cd /home
-wget -O dependency-check-6.0.3-release.zip https://dl.bintray.com/jeremy-long/owasp/dependency-check-6.0.3-release.zip
-unzip dependency-check-6.0.3-release.zip
-rm -f dependency-check-6.0.3-release.zip
+# echo '--install sonar-scanner--'
+# wget -O sonar-scanner-cli-4.2.0.1873.zip  https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873.zip
+# unzip sonar-scanner-cli-4.2.0.1873.zip -d /home
+# rm -f sonar-scanner-cli-4.2.0.1873.zip
+# cd /home
+# mv sonar-scanner-4.2.0.1873 sonar-scanner
 
 echo '--install openshift cli--'
 wget -O openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
@@ -41,5 +35,14 @@ echo '--install internal cert--'
 update-ca-certificates
 # keytool -import -storepass changeit -file /usr/share/ca-certificates/master.crt -keystore /etc/ssl/certs/java/cacerts -noprompt -alias jenkins_master
 
-echo '--downoad dependency database--'
-/home/dependency-check/bin/dependency-check.sh --scan .
+echo '--install sdkman manage java version--'
+curl -s "https://get.sdkman.io" | bash
+rm -rf /var/lib/apt/lists/*
+echo "sdkman_auto_answer=true" > $SDKMAN_DIR/etc/config
+echo "sdkman_auto_selfupdate=false" >> $SDKMAN_DIR/etc/config
+echo "sdkman_insecure_ssl=true" >> $SDKMAN_DIR/etc/config
+
+source $SDKMAN_DIR/bin/sdkman-init.sh
+
+sdk install java 7.0.282-zulu
+sdk install java 8.0.272-zulu
